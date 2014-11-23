fa301a77c758aac1a9ec57bd2d35a0f480c16c0b
Sean McGary <sean@seanmcgary.com>
2014-11-22 16:49:37 -0800

    users and tickets
    nest user data in { user: {} }
    
    nest ticket data - v0.0.3
    
    update ticket
    
    version bump
    
    update ticket
    
    stuff

--- example/index.js
+++ example/index.js
@@ -1,3 +1,15 @@
 var zendesk = require('../');
 
-console.log(zendesk);
+var client = new zendesk.Client({
+	username: '',
+	token: '',
+	host: '',
+	apiVersion: 'v2'
+});
+
+client.tickets().get(4104)
+.then(function(ticket){
+	console.log(ticket.ticket);
+}, function(){
+	console.log(arguments);
+});
